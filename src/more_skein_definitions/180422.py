98da6e1fb54acf314bdd7b01287142d354072815
CJ-Griffin <cj.griffin@protonmail.com>
2022-04-18 00:20:38 +0100

    New experiments 2

---
+++ src/more_skein_definitions/180422.py
@@ -0,0 +1,64 @@
+from src.run_parameters import TrainParams
+
+SKEIN_DICT = {
+    "MuseumRush": [
+        TrainParams(
+            env_name="MuseumRush",
+            agent_name="QLearner",
+            num_episodes=int(1e5),
+            gamma=0.9,
+            epsilon=0.05,
+            mu=mu,
+            dist_measure_name=D,
+        )
+        for D in ['vase_door', 'simple', "rgb"]
+        for mu in [0.0, 0.2, 0.4, 0.6, 0.8,
+                   1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+    ],
+
+    "EmptyDirtyRoom": [
+        TrainParams(
+            env_name="EmptyDirtyRoom",
+            agent_name="QLearner",
+            num_episodes=int(2e5),
+            gamma=0.9,
+            epsilon=0.05,
+            mu=mu,
+            dist_measure_name=D,
+        )
+        for D in ['vase_door', 'simple', "rgb"]
+        for mu in [0.0, 0.2, 0.4, 0.6, 0.8,
+                   1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+    ],
+
+    "EasyDoorGrid": [
+        TrainParams(
+            env_name="EasyDoorGrid",
+            agent_name="QLearner",
+            num_episodes=int(2e5),
+            gamma=0.9,
+            epsilon=0.05,
+            mu=mu,
+            dist_measure_name=D,
+        )
+        for D in ['vase_door', 'simple', "rgb"]
+        for mu in [0.0, 0.2, 0.4, 0.6, 0.8,
+                   1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+    ],
+
+    "SmallMuseumGrid": [
+        TrainParams(
+            env_name="SmallMuseumGrid",
+            agent_name="QLearner",
+            num_episodes=int(1e6),
+            gamma=0.9,
+            epsilon=0.05,
+            mu=mu,
+            dist_measure_name=D,
+        )
+        for D in ['vase_door', 'simple', "rgb"]
+        for mu in [0.0, 0.2, 0.4, 0.6, 0.8,
+                   1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+    ],
+
+}
