d9708162c459d147d4b298019f6dc1599a47fc18
CJ-Griffin <cj.griffin@protonmail.com>
2022-05-02 17:22:05 +0100

    New set of experiments

---
+++ src/more_skein_definitions/020522.py
@@ -0,0 +1,69 @@
+from src.run_parameters import TrainParams
+
+SKEIN_DICT = {
+
+    "NewEasyDoorGridShort": [
+        TrainParams(
+            env_name="EasyDoorGrid",
+            agent_name="QLearner",
+            num_episodes=int(1e5),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            is_test=True,
+            dist_measure_name=D,
+        )
+        for D in (['perf', 'simple', "rgb"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+    ],
+
+    "NewEasyDoorGridLong": [
+        TrainParams(
+            env_name="EasyDoorGrid",
+            agent_name="QLearner",
+            num_episodes=int(1e6),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            is_test=True,
+            dist_measure_name=D,
+        )
+        for D in (['perf', 'simple', "rgb"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+    ],
+
+    "NewSushiShort": [
+        TrainParams(
+            env_name="SushiGrid",
+            agent_name="QLearner",
+            num_episodes=int(1e5),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            is_test=True,
+            dist_measure_name=D,
+        )
+        for D in (['perf', 'simple', "rgb"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+    ],
+
+    "NewSushiLong": [
+        TrainParams(
+            env_name="SushiGrid",
+            agent_name="QLearner",
+            num_episodes=int(1e6),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            is_test=True,
+            dist_measure_name=D,
+        )
+        for D in (['perf', 'simple', "rgb"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+    ],
+
+}
