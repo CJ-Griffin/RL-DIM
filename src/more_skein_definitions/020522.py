f09ffb6ca39580be6b780b170ddf14df4fd98251
CJ-Griffin <cj.griffin@protonmail.com>
2022-05-03 09:27:35 +0100

    Test RR and rev

--- src/more_skein_definitions/020522.py
+++ src/more_skein_definitions/020522.py
@@ -61,7 +61,39 @@ SKEIN_DICT = {
         for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
     ],
 
-    "rr_comp": [
+    "rr_comp1": [
+        TrainParams(
+            env_name=env,
+            agent_name="QLearner",
+            num_episodes=int(1e5),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            dist_measure_name=D,
+        )
+        for D in (["RR"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+        for env in (["MuseumRush"])
+    ],
+
+    "rr_comp2": [
+        TrainParams(
+            env_name=env,
+            agent_name="QLearner",
+            num_episodes=int(1e5),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            dist_measure_name=D,
+        )
+        for D in (["RR"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+        for env in (["EasyDoorGrid"])
+    ],
+
+    "rr_comp3": [
         TrainParams(
             env_name=env,
             agent_name="QLearner",
@@ -74,7 +106,23 @@ SKEIN_DICT = {
         )
         for D in (["RR"])
         for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
-        for env in (["MuseumRush", "EasyDoorGrid", 'SmallMuseumGrid', "EmptyDirtyRoom"])
+        for env in (['SmallMuseumGrid'])
+    ],
+
+    "rr_comp4": [
+        TrainParams(
+            env_name=env,
+            agent_name="QLearner",
+            num_episodes=int(1e5),
+            gamma=1.0,
+            epsilon=0.1,
+            alpha=0.1,
+            mu=float(mu),
+            dist_measure_name=D,
+        )
+        for D in (["RR"])
+        for mu in ([0, 1, 2, 4, 8, 16, 32, 64])
+        for env in (["EmptyDirtyRoom"])
     ],
 
     "hooman": [
